767226c82616ca2b4b2b9fc65dd0189640d75ca2
Awhittington12 <awhittington12@ivytech.edu>
2026-07-31 09:15:15 -0400

    Delete final Project_7-2 directory

--- final Project_7-2/expense.py
+++
@@ -1,8 +0,0 @@
-class Expense:
-    def __init__(self, name, amount, category):
-        self.name = name
-        self.amount = float(amount)
-        self.category = category
-
-    def __str__(self):
-        return f"{self.name} - ${self.amount:.2f} ({self.category})"
